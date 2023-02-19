aec98013f0a2315b8b7b25445d5b651e52cb2490
“dariushbadakhshan” <dariush.badakhshan@gmail.com>
2023-02-19 14:24:06 +0330

    describe content of Navbar and Contnet of Header

--- src/components/Header/Header.Content.tsx
+++ src/components/Header/Header.Content.tsx
@@ -1,4 +1,15 @@
 const Content = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h1>Order Delicios Food</h1>
+        <h2>Delivered TO You</h2>
+        <p>
+          Choose your favorite meal from our broad selection of available meales
+          and enjoy a delicious luch or dinner at home.
+        </p>
+      </div>
+    </div>
+  );
 };
 export default Content;
