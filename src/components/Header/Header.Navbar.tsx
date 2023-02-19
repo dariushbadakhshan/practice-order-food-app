b2a89c14c1d2d4c24be2bf5d3d0f83256873cab8
“dariushbadakhshan” <dariush.badakhshan@gmail.com>
2023-02-19 14:14:57 +0330

    add Header into App component

--- src/components/Header/Header.Navbar.tsx
+++ src/components/Header/Header.Navbar.tsx
@@ -1,4 +1,13 @@
 const Navbar = () => {
-  return <nav></nav>;
+  return (
+    <nav>
+      <div>
+        <p>Omnia Food</p>
+      </div>
+      <div>
+        <button>Your Card</button>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
